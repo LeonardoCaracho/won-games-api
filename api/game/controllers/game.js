056034b951be8e3e82c4ee87ecc5747175e07d71
Leonardo Caracho <leeo.apc@gmail.com>
2020-11-16 17:22:23 -0300

    move api to this repo

---
+++ api/game/controllers/game.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = {
+  populate: async (ctx) => {
+    console.log("starting to populate...");
+
+    const options = {
+      sort: "popularity",
+      page: "1",
+      ...ctx.query,
+    };
+
+    await strapi.services.game.populate(options);
+    ctx.send("Finished populating");
+  },
+};
